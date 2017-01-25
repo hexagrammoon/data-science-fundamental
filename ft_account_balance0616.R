f72747f3e7e63980ff385c0290989069bfdd864d
nightever <514045191@qq.com>
2017-01-25 17:48:44 +0000

    Update ft_account_balance0616.R

--- ft_account_balance0616.R
+++ ft_account_balance0616.R
@@ -1,6 +1,7 @@
+# load library packages
 library(data.table)
 library(dplyr)
 
-
+# read the data
 ft_account_balance<-fread("ft_account_balance0616.txt", header = TRUE, fill = TRUE)
 str(ft_account_balance)
